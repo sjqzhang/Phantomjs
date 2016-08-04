9f728472d5e2bce406d0ddc48630b21a8737974e
s-jqzhang <s-jqzhang@meizu.com>
2016-08-04 10:21:52 +0800

    kankandou tpl

--- controllers/KanKanDou.py
+++ controllers/KanKanDou.py
@@ -5,28 +5,79 @@ from codeigniter import ci
 
 
 import Queue
+'''
+
+# step one: get_urls
+# step two: get_details
+# step three: download_files
 
 
 
+'''
+
+import inspect
+
+def __name__():
+    return inspect.stack()[1][3]
 
 class KanKanDou(object):
-    _queue=Queue.Queue(10000)
-    def init(self,req,resp):
+
+    def __init__(self):
+        self.tasks={}
+
+    def get_urls(self,req,resp):
+        if self.tasks.get(__name__())!=None:
+            return '%s has start'%(__name__())
+        else:
+            self.tasks[__name__()]=__name__()
+
         import gevent
         jobs=[]
-        for i in range(0,60):
-            jobs.append(gevent.spawn(self.get_urls))
+        queue=Queue.Queue(10000)
         for i in range(2,404):
-            self._queue.put(i)
+            queue.put(i)
+        for i in range(0,20):
+            jobs.append(gevent.spawn(self._get_urls,queue))
         gevent.joinall(jobs)
 
 
-    def get_urls(self):
+    def get_details(self,req,resp):
+        if self.tasks.get(__name__())!=None:
+            return '%s has start'%(__name__())
+        else:
+            self.tasks[__name__()]=__name__()
+        import gevent
+        jobs=[]
+        queue=Queue.Queue(1000000)
+        for i in range(0,20):
+            jobs.append(gevent.spawn(self._get_details,queue))
+        rows= ci.db.query("select * from urls where status=0 and site='KanKanDou'")
+        for row in rows:
+            queue.put(row)
+        gevent.joinall(jobs)
+
+    def download(self,req,resp):
+        if self.tasks.get(__name__())!=None:
+            return '%s has start'%(__name__())
+        else:
+            self.tasks[__name__()]=__name__()
+        import gevent
+        queue=Queue.Queue(1000000)
+        rows= ci.db.query("select * from files where status=0 and site='KanKanDou'")
+        for row in rows:
+            queue.put(row)
+        jobs=[]
+        for i in range(0,20):
+            jobs.append(gevent.spawn(self._download_files,queue))
+        gevent.joinall(jobs)
+
+
+    def _get_urls(self,queue):
         while True:
             import requests
             import json
             try:
-                i=self._queue.get(timeout=3)
+                i=queue.get(timeout=3)
                 if i!=None:
                     try:
                         import requests
@@ -51,14 +102,13 @@ class KanKanDou(object):
                 pass
 
 
-    def down_file(self):
+    def _get_details(self,queue):
         import time
         while True:
-            rows= ci.db.query("select * from urls where status=0 and site='KanKanDou' ORDER BY RANDOM()  limit 1 ")
-            import requests
-            import json
-            if len(rows)>0:
-                row=rows[0]
+            try:
+                row=queue.get(timeout=30)
+                import requests
+                import json
                 try:
                     import requests
                     import json
@@ -80,38 +130,20 @@ class KanKanDou(object):
                         ci.db.update('urls',{'status':1}, {'href':row['href']})
                         # time.sleep(0.001)
                 except Exception as er:
-                    pass
-                    #ci.logger.error(er)
-            else:
-                break
+                    #pass
+                    ci.logger.error(er)
+            except Exception as er:
+                pass
 
 
 
-    def start(self,req,resp):
-        import gevent
-        # joblist=[lambda x:gevent.spawn(self.down_file) for i in range(0,40)]
-        jobs=[]
-        for i in range(0,40):
-            jobs.append(gevent.spawn(self.down_file))
-        gevent.joinall(jobs)
-
-    def download(self,req,resp):
-        import gevent
-        # joblist=[lambda x:gevent.spawn(self.down_file) for i in range(0,40)]
-        jobs=[]
-        for i in range(0,20):
-            jobs.append(gevent.spawn(self._download))
-        gevent.joinall(jobs)
-
-
-    def _download(self):
+    def _download_files(self,queue):
         import time
+        import requests
+        import json
         while True:
-            rows= ci.db.query("select * from files where status=0 and site='KanKanDou' ORDER BY RANDOM()  limit 1 ")
-            import requests
-            import json
-            if len(rows)>0:
-                row=rows[0]
+            try:
+                row=queue.get(timeout=30)
                 try:
                     r = requests.get(row['href'], stream=True)
                     with open("H:/kankandou/"+ row['title'], 'wb') as f:
@@ -120,11 +152,9 @@ class KanKanDou(object):
                                 f.write(chunk)
                                 f.flush()
                         f.close()
-
                     ci.db.update('files',{'status':1}, {'href':row['href']})
-
                 except Exception as er:
                     pass
-                    #ci.logger.error(er)
-            else:
-                break
+                    ci.logger.error(er)
+            except Exception as er:
+                pass
