8d7a6fa0f6dd9b904e1dc37d6131d9579f8c4ed8
小张 <s_jqzhang@163.com>
2018-06-02 18:02:22 +0800

    Add files via upload
    add entry.js

---
+++ static/entry.js
@@ -0,0 +1,310 @@
+const express = require("express")
+const app = express()
+const path = require('path')
+const bodyParser  = require("body-parser");
+const puppeteer = require('puppeteer');
+
+
+
+function hereDoc(f) {
+    return f.toString().replace(/^[^\/]+\/\*!?/, '').replace(/\*\/[^\/]+$/, '')
+}
+String.prototype.trim=function()
+{
+     return this.replace(/(^\s*)|(\s*$)/g,'');
+}
+
+function isEmpty(e) {
+    var t;
+    for (t in e)
+        return !1;
+    return !0
+}
+
+
+
+function tpl_replace(str,data){
+    var tpl=str
+    for(var key in data){
+        tpl=tpl.replace('{'+key+'}',data[key])
+    }
+    return tpl
+}
+
+
+function _PYTHON_REQUEST(){
+/*
+for i in range(0,1):
+    try:
+        import requests
+        import json
+        url='''{url}'''
+        header='''{header}'''
+        body='''{body}'''
+        jscode='''{jscode}'''
+        posturl='''{posturl}'''#js server phantomjs
+        data={'url':url,'header':header,'body':body,'jscode':jscode,'posturl':posturl}
+        jdata=requests.post('http://127.0.0.1:8080/api/request',data).text
+        jdata=json.loads(jdata)
+        for d in jdata:
+            d['site']='KanKanDou'
+            d['status']='0'
+            d['level']='1'
+            print d
+            #ci.db.insert('urls',d)
+    except Exception as er:
+        pass
+        #ci.logger.error(er)
+*/
+}
+
+function obj2string(o) {
+    var r = [];
+    if (typeof o == "string") {
+        return "\"" + o.replace(/([\'\"\\])/g, "\\$1").replace(/(\n)/g, "\\n").replace(/(\r)/g, "\\r").replace(/(\t)/g, "\\t") + "\""
+    }
+    if (typeof o == "object") {
+        if (o == null) {
+            return 'null'
+        }
+        if (!o.sort) {
+            for (var i in o) {
+                r.push(i + ":" + obj2string(o[i]))
+            }
+            if ( !! document.all && !/^\n?function\s*toString\(\)\s*\{\n?\s*\[native code\]\n?\s*\}\n?\s*$/.test(o.toString)) {
+                r.push("toString:" + o.toString.toString())
+            }
+            r = "{" + r.join() + "}"
+        } else {
+            for (var i = 0; i < o.length; i++) {
+                r.push(obj2string(o[i]))
+            }
+            r = "[" + r.join() + "]"
+        }
+        return r
+    }
+    return o.toString()
+}
+
+
+function valid_head(str){
+    if(str==undefined){
+      return false;
+    } else {
+        check=function(str,sep) {
+            var lines = str.split(/\n/);
+            var flag = true;
+            for (var i = 0; i < lines.length; i++) {
+                var line = lines[i];
+                if (line.trim() != '') {
+                    var pos = line.indexOf(sep);
+                    if (pos == -1) {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            return flag
+        }
+        return check(str,':')||check(str,'=')
+    }
+
+}
+
+function build_header(str){
+    if(str==undefined||!valid_head(str)){
+       return []
+    }
+    var header=[];
+    var lines= str.split(/\n/);
+    for(var i=0;i<lines.length;i++){
+        var line=lines[i];
+        if(line.trim()!=''){
+            var pos= line.indexOf(':')>0?line.indexOf(':'):line.indexOf('=');
+            if(pos!=-1){
+                var key=line.substring(0,pos).trim()
+                var value=line.substring(pos+1,line.length).trim()
+                header.push({'key':key,'value':value})
+            }
+        }
+    }
+    return header;
+}
+
+function get_kv(items){
+   var kv={}
+    for(var i=0;i<items.length;i++){
+          var item=items[i];
+          kv[item['key']]=item['value'];
+    }
+    return kv;
+}
+
+
+function build_data(str) {
+    var data=[];
+    if(str==undefined){
+        return data
+    }
+    var values=str.split('&');
+    for(var i=0;i<values.length;i++) {
+        var value=values[i];
+        var kvs=value.split('=')
+        if(kvs.length==2){
+            data.push({'key':kvs[0],'value':decodeURIComponent(kvs[1])})
+        }
+    }
+    return data
+}
+function waitFor(testFx, onReady, onTimeout, timeOutMillis) {
+    var maxtimeOutMillis = timeOutMillis ? timeOutMillis: 5000,
+    start = new Date().getTime(),
+    condition = false,
+    interval = setInterval(function() {
+        if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
+            condition = (typeof(testFx) === "string" ? eval(testFx) : testFx())
+        } else {
+            if (!condition) {
+                console.log("'waitFor()' timeout");
+                typeof(onTimeout) === "string" ? eval(onTimeout) : onTimeout();
+                clearInterval(interval)
+            } else {
+                console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                typeof(onReady) === "string" ? eval(onReady) : onReady();
+                clearInterval(interval)
+            }
+        }
+    },
+    250)
+};
+
+var fs = require('fs');
+function get_file_content(filename) {
+    var content = fs.read(filename);
+    return content;
+}
+function isEmptyObject(obj) {
+    var name;
+    for (name in obj) {
+        return false
+    }
+    return true
+}
+
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+
+app.use(express.static(path.join(__dirname, '.')))
+
+// browser = puppeteer.launch();
+
+
+
+app.post("/api/buildRequest", function (req, res) {
+	
+	var doc=hereDoc(_PYTHON_REQUEST)
+	var data={'body':req.body.body,'header':req.body.header,'js':req.body.js,
+	'jscode':req.body.jscode}
+	doc=tpl_replace(doc,req.body)
+	res.send(doc)
+	
+})
+
+app.post("/api/request", function (req, res) {
+  console.log("req", req.url);
+  (async (req,res) => {
+     const browser =await puppeteer.launch({devtools: true});
+    // const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    // await page.goto('https://example.com');
+    
+
+    // console.log(req.body.jscode)
+
+    const jscode= await req.body.jscode
+	
+	const url= await req.body.url
+	
+	const headers= await req.body.header
+	
+	//console.log(headers)
+	
+	const header=get_kv(build_header(headers))
+	
+	
+	
+	
+	try{
+		var cookies=header['Cookie']||header['cookie']||''
+		
+		var cc=cookies.split(';')
+		var cookies_array=[]
+		for(var i in cc) {
+			
+			var kv=cc[i].split('=')
+			if(kv.length>1) {
+				cookies_array.push({
+				  'name'     : kv[0],   /* required property */
+				  'value'    : kv[1],  /* required property */
+				  //'domain'   : 'localhost',
+				 // 'domain'	: 'gitlab.fenqile.com',
+				  'path'     : '/',                /* required property */
+				  'httponly' : true,
+				  //'secure'   : false,
+				  'expires'  : (new Date()).getTime() + (1000 * 60 * 60)   /* <-- expires in 1 hour */
+				});
+			
+			}
+		}
+		//console.log(cookies_array)
+		//await page.setCookie('Cookie',cookies_array)
+	} catch(e) {
+		
+		console.log("xxxxxxxxxxxxx",e)
+	}
+  
+   // page.on('request', interceptedRequest => {
+   //   if (interceptedRequest.resourceType()=='script'){
+   //        console.log("js",interceptedRequest.url())
+   //        interceptedRequest.continue();
+   //    } else {
+   //    interceptedRequest.abort()
+   //    }
+   //  });
+
+    await page.goto(req.body.url);
+
+    await page.addScriptTag({url: "http://127.0.0.1:3000/jquery.js"});
+    await page.addScriptTag({url: "http://127.0.0.1:3000/util.js"});
+
+
+
+    const message = await page.evaluate(function (jscode){
+
+            return jscode
+    }(jscode));
+
+    // await console.log(message)
+
+    await res.send(JSON.stringify(message))
+
+    console.log(message)
+    // await page.screenshot({path: './public/example.png'});
+
+  //  await browser.close();
+  })(req,res);
+
+  // res.send("taking screenshot")
+});
+
+
+
+app.listen(3000, function () {
+  console.log("listening on port 3000!")
+})
